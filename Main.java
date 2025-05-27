cef2631469a2ddb72e653ac858bf7eed9a3c54c4
Raphael "CapGyb" Silva Brito <raphasilvabrito@gmail.com>
2025-05-27 11:16:49 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        EscritorSimples.main();
+        LeitorSimples.main();
+
+        System.out.println();
+
+        GravadorDoClientes.main();
+        LeitorDoClientes.main();
+    }
+}
